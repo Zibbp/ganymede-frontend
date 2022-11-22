d0940aacb4176289321e6e9ac2c937d3b386d911
Zibbp <zibbp@protonmail.com>
2022-11-21 20:37:48 -0600

    build(next): remove env next_public

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   publicRuntimeConfig: {
-    API_URL: process.env.NEXT_PUBLIC_API_URL,
-    CDN_URL: process.env.NEXT_PUBLIC_CDN_URL,
-    SHOW_SSO_LOGIN_BUTTON: process.env.NEXT_PUBLIC_SHOW_SSO_LOGIN_BUTTON,
-    FORCE_SSO_AUTH: process.env.NEXT_PUBLIC_FORCE_SSO_AUTH,
+    API_URL: process.env.API_URL,
+    CDN_URL: process.env.CDN_URL,
+    SHOW_SSO_LOGIN_BUTTON: process.env.SHOW_SSO_LOGIN_BUTTON,
+    FORCE_SSO_AUTH: process.env.FORCE_SSO_AUTH,
   },
   reactStrictMode: true,
   swcMinify: true,
