ecbf0cf91dd1c5dffc1d3830b70351533cf5c1cd
Zibbp <zibbp@protonmail.com>
2022-11-21 20:04:56 -0600

    feat(next): runtime vars

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_CDN_URL: process.env.NEXT_PUBLIC_CDN_URL,
+    NEXT_PUBLIC_SHOW_SSO_LOGIN_BUTTON:
+      process.env.NEXT_PUBLIC_SHOW_SSO_LOGIN_BUTTON,
+    NEXT_PUBLIC_FORCE_SSO_AUTH: process.env.NEXT_PUBLIC_FORCE_SSO_AUTH,
+  },
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
