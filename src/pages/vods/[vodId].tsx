61ad55fc2e93de6d54217aeb33603ccff1afb348
Zibbp <zibbp@protonmail.com>
2023-01-22 14:48:19 -0600

    feat(vod): log in required check

--- src/pages/vods/[vodId].tsx
+++ src/pages/vods/[vodId].tsx
@@ -1,16 +1,15 @@
 import { createStyles, Grid } from "@mantine/core";
 import { useDocumentTitle } from "@mantine/hooks";
 import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import { VodChatPlayer } from "../../components/Vod/ChatPlayer";
 import ExperimentalChatPlayer from "../../components/Vod/ExperimentalChatPlayer";
 import { VodTitleBar } from "../../components/Vod/TitleBar";
-import { VodVideoPlayer } from "../../components/Vod/VideoPlayer";
 import { useApi } from "../../hooks/useApi";
 import useUserStore from "../../store/user";
 import Error from "next/error";
-import dynamic from "next/dynamic";
 import NewVideoPlayer from "../../components/Vod/NewVideoPlayer";
+import getConfig from "next/config";
+import VodLoginRequired from "../../components/Vod/LoginRequred";
 
 const useStyles = createStyles((theme) => ({
   videoPlayerColumn: {
@@ -42,6 +41,8 @@ export async function getServerSideProps(context: any) {
 
 const VodPage = (props: any) => {
   const { classes, cx, theme } = useStyles();
+  const { publicRuntimeConfig } = getConfig();
+  const user = useUserStore((state) => state);
 
   useDocumentTitle(`Ganymede - VOD ${props.vodId}`);
 
@@ -50,30 +51,41 @@ const VodPage = (props: any) => {
     queryFn: () => fetchVod(props.vodId),
   });
 
+  const checkLoginRequired = () => {
+    if (publicRuntimeConfig.REQUIRE_LOGIN && !user.isLoggedIn) {
+      return true;
+    }
+    return false;
+  };
+
   if (!data) {
     return <Error statusCode={404} />;
   }
 
   return (
     <div>
-      <Grid columns={12} gutter={0}>
-        <Grid.Col className={classes.videoPlayerColumn} span="auto">
-          {/* <VodVideoPlayer vod={data} /> */}
-          <NewVideoPlayer vod={data} />
-        </Grid.Col>
-        {data.chat_video_path &&
-          !useUserStore.getState().settings.useNewChatPlayer && (
-            <Grid.Col className={classes.videoPlayerColumn} span={2}>
-              <VodChatPlayer vod={data} />
+      {checkLoginRequired() && <VodLoginRequired {...data} />}
+      {!checkLoginRequired() && (
+        <div>
+          <Grid columns={12} gutter={0}>
+            <Grid.Col className={classes.videoPlayerColumn} span="auto">
+              <NewVideoPlayer vod={data} />
             </Grid.Col>
-          )}
-        {data.chat_path &&
-          useUserStore.getState().settings.useNewChatPlayer && (
-            <Grid.Col className={classes.videoPlayerColumn} span={2}>
-              <ExperimentalChatPlayer vod={data} />
-            </Grid.Col>
-          )}
-      </Grid>
+            {data.chat_video_path &&
+              !useUserStore.getState().settings.useNewChatPlayer && (
+                <Grid.Col className={classes.videoPlayerColumn} span={2}>
+                  <VodChatPlayer vod={data} />
+                </Grid.Col>
+              )}
+            {data.chat_path &&
+              useUserStore.getState().settings.useNewChatPlayer && (
+                <Grid.Col className={classes.videoPlayerColumn} span={2}>
+                  <ExperimentalChatPlayer vod={data} />
+                </Grid.Col>
+              )}
+          </Grid>
+        </div>
+      )}
       <VodTitleBar vod={data} />
     </div>
   );
