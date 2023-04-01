d1cfcf56756278f27cb12694e3ca9e889e68c21e
Zibbp <zibbp@protonmail.com>
2023-04-01 13:23:21 -0500

    fix(vod/info): show raw json for vod info

--- src/components/Vod/InfoModalContent.tsx
+++ src/components/Vod/InfoModalContent.tsx
@@ -23,60 +23,8 @@ export const VodInfoModalContent = ({ vod }: any) => {
   return (
     <div>
       <div>
-        Id: <Code>{vod.id}</Code>
+        <Code block>{JSON.stringify(vod, null, 2)}</Code>
       </div>
-      <div>
-        External Id: <Code>{vod.ext_id}</Code>
-      </div>
-      <div>
-        Platform: <Code>{vod.platform}</Code>
-      </div>
-      <div>
-        Type: <Code>{vod.type}</Code>
-      </div>
-      <div>
-        Duration: <Code>{vod.duration}</Code>
-      </div>
-      <div>
-        Views: <Code>{vod.views}</Code>
-      </div>
-      <div>
-        Resolution: <Code>{vod.resolution}</Code>
-      </div>
-      <div>
-        Thumbnail Path: <Code>{vod.thumbnail_path}</Code>
-      </div>
-      <div>
-        Web Thumbnail Path: <Code>{vod.web_thumbnail_path}</Code>
-      </div>
-      <div>
-        Video Path: <Code>{vod.video_path}</Code>
-      </div>
-      <div>
-        Chat Path: <Code>{vod.chat_path}</Code>
-      </div>
-      <div>
-        Chat Video Path: <Code>{vod.chat_video_path}</Code>
-      </div>
-      <div>
-        Info Path: <Code>{vod.info_path}</Code>
-      </div>
-      <div>
-        Streamed At: <Code>{vod.streamed_at}</Code>
-      </div>
-      <div>
-        Created/Archived At: <Code>{vod.created_at}</Code>
-      </div>
-      {vod.edges.channel && (
-        <div>
-          <div>
-            Channel: <Code>{vod.edges.channel.name}</Code>
-          </div>
-          <div>
-            Channel Id: <Code>{vod.edges.channel.id}</Code>
-          </div>
-        </div>
-      )}
       <div>
         <Text mt={15} size="lg" weight={500}>
           Video FFprobe
